a4193559e953013b5ac0cc4d387444a08cef4cdd
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-06-16 22:40:41 -0500

    Renamed the file main.rs in Tutorial1 to tutorial_1.rs in tutorial_1! Ran the cargo new tutorial_2 command that created a new rust project! Updated the main.rs file in tutorial_2/src that contains comments about the commands learned so far!

---
+++ tutorial_2/src/main.rs
@@ -0,0 +1,8 @@
+// cargo new "project_name" - creates a new rust project
+// cargo build - compiles the rust project
+// cargo run - compiles and automatically runs the compiled rust project
+// cargo check - runs a test for bugs and compiles rust project without actually compiling
+// cd src, run "rustfmt main.rs" to formate a rust file!
+fn main() {
+    println!("Hello, world!");
+}
