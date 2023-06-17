20db80b6b97beab499c99b0c46a4e7d3a80d2697
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-06-16 21:41:43 -0500

    Added main.rs file with simple main function that prints Hello World! to the console!

---
+++ main.rs
@@ -0,0 +1,7 @@
+// fn = function
+// main() is executed automatically when ran
+fn main() {
+    // Recommended to have four spaces and not tab
+    // print line ! (! means macro)
+    println!("hello world!");
+}
